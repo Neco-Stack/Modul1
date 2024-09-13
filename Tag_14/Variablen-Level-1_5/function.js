f1ff8f02b0b8b3c748e67ed83bbd4a148c394ebb
Neco_Stack <necoabay@googlemail.com>
2024-09-13 13:40:03 +0200

    erste Aufgabe variables

--- Tag_14/Variablen-Level-1_5/function.js
+++ Tag_14/Variablen-Level-1_5/function.js
@@ -1,1 +1,14 @@
-let x; 
+let carName;
+carName="BMW"
+console.log(carName);
+let x = 150
+let y = 50
+let z = x+y 
+let firstName = "John"
+let lastName = "Doe"
+let age = 35 
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(firstName)
+console.log(age)
