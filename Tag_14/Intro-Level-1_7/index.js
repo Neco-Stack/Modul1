8997dc9382203416fb8255f9959cc6a2cc38e091
Neco_Stack <necoabay@googlemail.com>
2024-09-13 16:03:10 +0200

    last task variables

---
+++ Tag_14/Intro-Level-1_7/index.js
@@ -0,0 +1,16 @@
+let helloWorld = "Hello World";
+console.log(helloWorld);
+const pi = 3.14;
+console.log(pi); 
+const burjDubai = 828 + "m"
+console.log(burjDubai);
+const fullName = "Jan Schmidt"
+console.log(fullName);
+const eifelTower = "384m"
+console.log(eifelTower)
+let camelCase= "Camel Case)"
+console.log(camelCase) 
+let kursStatus = "true"
+console.log(kursStatus);
+
+console.log(helloWorld + pi + burjDubai + fullName + eifelTower + camelCase + kursStatus)
