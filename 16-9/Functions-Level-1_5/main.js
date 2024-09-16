ef4bfd8a64207091a893e2cdf8a5ff8bdd5b1f3c
Neco_Stack <necoabay@googlemail.com>
2024-09-16 14:54:11 +0200

    multiply

---
+++ 16-9/Functions-Level-1_5/main.js
@@ -0,0 +1,13 @@
+function math(x,y){
+    console.log("Multiplikation", x * y);
+    console.log("Division", x/y);
+}
+math(10,2);
+math(30,20);
+math(100,200);
+math(5,0)
+math(45,173);
+math(1,1000);
+
+// hier wurde eine Funktion mit zwei Parametern erstellt. In der Konsole
+// wurden diese Werte multipliziert/ dividiert)
