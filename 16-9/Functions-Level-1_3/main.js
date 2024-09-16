d77b18810a5c68b462035a150f2ad7a3a9c77690
Neco_Stack <necoabay@googlemail.com>
2024-09-16 14:30:16 +0200

    function

---
+++ 16-9/Functions-Level-1_3/main.js
@@ -0,0 +1,6 @@
+function intro3(name, stadt, alter){
+     
+    console.log("Hello mein Name ist " + name + ". Ich bin" + alter + ". Ich komme aus " + stadt)
+}
+intro3("Necati", "Düsseldorf", "37");
+
