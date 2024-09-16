1597d44cb1bc31a1b7d5a748d31075728d22aacc
Neco_Stack <necoabay@googlemail.com>
2024-09-16 21:17:40 +0200

    upload

---
+++ 16-9/Dom-Level-1_3/assets/js/main.js
@@ -0,0 +1,16 @@
+const gallery = document.getElementById ("gallery");
+if (!gallery){
+    console.error
+}
+const figuresHTML =
+    <figures>
+        <img src="./assets/img/bild.jpg" alt="">
+        <figcaption>fig1</figcaption>
+    </figures>
+
+
+
+
+
+// die id "gallery" zunächst als constante definiert
+// geprüft ob die id "gallery" im HTML existiert
