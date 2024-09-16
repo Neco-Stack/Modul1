a13a8f2335292cf495c05e1a9654af3a8a6ce624
Neco_Stack <necoabay@googlemail.com>
2024-09-16 15:38:56 +0200

    dom task 1

---
+++ 16-9/DOM-Level-1_2/main.js
@@ -0,0 +1,5 @@
+document.getElementById("info").innerHTML = 
+"<h1>Hello World</> <h2>How are you?</>"
+document.getElementById("container").innerHTML = 
+"<p>start of the Element</>"
+document.write("end of the the element")
