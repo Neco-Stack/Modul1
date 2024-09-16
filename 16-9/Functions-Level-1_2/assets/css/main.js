70f23d8a7277048b749032d4218c412161268a43
Neco_Stack <necoabay@googlemail.com>
2024-09-16 14:04:09 +0200

    functions intro

---
+++ 16-9/Functions-Level-1_2/assets/css/main.js
@@ -0,0 +1,10 @@
+function intro2(paramName = "King"){
+    let name = "SuperCoder:in"; 
+    console.log("Hello " + name + "mein Name ist " + paramName)
+}
+intro2();
+
+// hier wurde eine Funktion "Intro2" erschaffen mit dem Parameter "paramName". 
+// eine Variable "name" erstellt mit dem Wert "SuperCode"
+// Eine Nachricht in der Konsole mit der variable und dem parameter
+// intro2() führt in der Konsole die Funktion aus und Parameter kann in Klammer eingegeben werden
